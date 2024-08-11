ddbc948f0538f434e6ac55693637f894d9bbbd37
Alexandre <alexandre.ealimentos@gmail.com>
2024-08-11 18:26:17 +0900

    Added basic methods to test flask app

--- hello.py
+++ hello.py
@@ -1,33 +1,45 @@
 from flask import Flask, redirect, url_for, request
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello_world():
-    if request.method == 'POST':
-        user = request.form['name']
-        if user != '':
-            return redirect(url_for('greet', name=user))
-        else:
-            return f'''
-            <p>Name cannot be empty!</p><br>
-            <form action="/">
-            <input type="submit" value="Go Home">
-        </form>
-        '''
-            
-    return '''
-        <form method="POST">
-            Name: <input type="text" name="name"><br>
-            <input type="submit" value="Submit"><br>
-        </form>
-    '''
+# Routing
+@app.route("/")
+def index():
+    return "Index Page"
+# Testing <variables>
+@app.route("/hello-world")
+def hello():
+    return "Hello, World!"
+@app.route("/user/<username>")
+def show_user_profile(username):
+    return f'User ({escape(username)})'
 
-@app.route("/greet/<name>")
-def greet(name):
-    return f'''
-        <p>Hello, {name}!</p>
-        <form action="/">
-            <input type="submit" value="Go Home">
-        </form>
-    '''
+@app.route("/post/<post_id>")
+def show_post_id(post_id):
+    return f'Post ({escape(post_id)})'
+
+# Unique URLs/Redirection
+@app.route("/projects/")
+def projects():
+    return "Projects Page"
+
+@app.route("/about")
+def about():
+    return "About Page"
+
+# HTTP methods
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+    
+@app.get('/login')
+def login_get():
+    return show_the_login_form()
+
+@app.post('/login')
+def login_post():
+    return do_the_login()
